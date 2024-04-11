6a51420d6e61fe4893fcac8516d674754c2d95e9
王童 <785271992@qq.com>
2024-04-11 20:20:36 +0800

    update

--- bt/builder.py
+++ bt/builder.py
@@ -7,6 +7,7 @@ from bt.nodes import *
 class CustomBTBuilder(BTPolicyBuilder):
 
     def register_default(self):
+        super().register_default()
         # 在这里注册新的行为节点，不然没办法在xml中使用
         self.register_node(
                 TestNode
